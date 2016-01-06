4bc4d3c51282cfef3cb6d1d3ac371ee2a2aa3949
Bill Fero <billfero@gmail.com>
2016-01-06 17:42:45 +0000

    Add radio buttons to the stock chart, select the particular chart you want to download and display.

--- app/src/main/java/com/example/marketdatatracker/ui/fragments/StockChartFragment.java
+++ app/src/main/java/com/example/marketdatatracker/ui/fragments/StockChartFragment.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.marketdatatracker.R;
 import com.example.marketdatatracker.event.AppMessageEvent;
@@ -19,13 +21,21 @@ import com.example.marketdatatracker.network.GetStockChartThread;
 
 import de.greenrobot.event.EventBus;
 
-public class StockChartFragment extends Fragment{
+public class StockChartFragment extends Fragment implements View.OnClickListener{
 
     private static final String INTERVAL = "5d";
     private String mSymbol;
     private ImageView mStockChart;
     private ProgressBar mProgressBar;
     private View mView;
+    private RadioGroup mRadioGroupVertical;
+    private RadioGroup mRadioGroupHorizontal;
+    private RadioButton mButton_1d;
+    private RadioButton mButton_5d;
+    private RadioButton mButton_1m;
+    private RadioButton mButton_3m;
+    private RadioButton mButton_1y;
+    private RadioButton mButton_5y;
 
     public StockChartFragment() {}
 
@@ -43,13 +53,17 @@ public class StockChartFragment extends Fragment{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
     }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.stock_item_chart, container, false);
 
+        // setup the radio buttons
+        setupRadioButtons();
         mProgressBar = (ProgressBar) mView.findViewById(R.id.progress_bar);
         mStockChart = (ImageView) mView.findViewById(R.id.stock_chart);
 
@@ -58,10 +72,59 @@ public class StockChartFragment extends Fragment{
 
         // fetch the stock obj from the data cache & download the
         mSymbol = getArguments().getString(StockDataCache.STOCK_OBJECT);
-        new GetStockChartThread(mSymbol, INTERVAL).start();
+        if(savedInstanceState == null) {
+            new GetStockChartThread(mSymbol, INTERVAL).start();
+        }
+        else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            setStockChart();
+        }
 
         return mView;
     }
+
+
+    private void setupRadioButtons() {
+        mButton_1d = (RadioButton) mView.findViewById(R.id.stock_1d);
+        mButton_5d = (RadioButton) mView.findViewById(R.id.stock_5d);
+        mButton_1m = (RadioButton) mView.findViewById(R.id.stock_1m);
+        mButton_3m = (RadioButton) mView.findViewById(R.id.stock_3m);
+        mButton_1y = (RadioButton) mView.findViewById(R.id.stock_1y);
+        mButton_5y = (RadioButton) mView.findViewById(R.id.stock_5y);
+
+        mButton_1d.setOnClickListener(this);
+        mButton_5d.setOnClickListener(this);
+        mButton_1m.setOnClickListener(this);
+        mButton_3m.setOnClickListener(this);
+        mButton_1y.setOnClickListener(this);
+        mButton_5y.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.stock_1d:
+                new GetStockChartThread(mSymbol, "1d").start();
+                break;
+            case R.id.stock_5d:
+                new GetStockChartThread(mSymbol, "5d").start();
+                break;
+            case R.id.stock_1m:
+                new GetStockChartThread(mSymbol, "1m").start();
+                break;
+            case R.id.stock_3m:
+                new GetStockChartThread(mSymbol, "3m").start();
+                break;
+            case R.id.stock_1y:
+                new GetStockChartThread(mSymbol, "1y").start();
+                break;
+            case R.id.stock_5y:
+                new GetStockChartThread(mSymbol, "5y").start();
+                break;
+        }
+    }
+
 
 
     @Override
@@ -83,13 +146,16 @@ public class StockChartFragment extends Fragment{
         mProgressBar.setVisibility(View.GONE);
 
         if(message.equals(AppMessageEvent.STOCK_DOWNLOAD_COMPLETE)) {
-            Drawable chart = StockDataCache.getStockDataCache().getStock(mSymbol).getPriceChart();
-            mStockChart.setImageDrawable(chart);
-            mStockChart.setVisibility(View.VISIBLE);
+            setStockChart();
         } else if(message.equals(AppMessageEvent.STOCK_DOWNLOAD_FAILED)) {
             Snackbar.make(mView, message, Snackbar.LENGTH_SHORT).show();
         }
     }
 
+    private void setStockChart() {
+        Drawable chart = StockDataCache.getStockDataCache().getStock(mSymbol).getPriceChart();
+        mStockChart.setImageDrawable(chart);
+        mStockChart.setVisibility(View.VISIBLE);
+    }
 
 }
